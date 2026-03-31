cafa6760b0014a22291544fd7696dab1a1b05c52
nibor07 <nibor07@gmail.com>
2026-03-31 04:15:38 -0300

    fix: app.module imports and AppController

--- services/contentgen-nestjs/src/app.module.ts
+++ services/contentgen-nestjs/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
 import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [...],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    JobsModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
